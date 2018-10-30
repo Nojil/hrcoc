344fe8bf68b80efa737e166ed1f2f81d3262ec95
Charles Lang <Charles.Lang@pscu.org>
2018-10-30 14:06:08 -0600

    Firebase Further Setup

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-footer.component.css']
 })
 export class AppFooterComponent implements OnInit {
+  showDetailsAdults = false;
+  showDetailsKids = false;
+  showDetailsEveryone = false;
+  showDetailsBaptism = false;
+  showDetailsCommunion = false;
+  showDetailsStudy = false;
+  showDetailsLamb = false;
+  showDetailsMissions = false;
+  showDetailsGiving = false;
+  showDetailsElders = false;
+  showDetailsBelieve = false;
+  showDetailsServices = false;
+
   constructor() { }
 
   ngOnInit() {
